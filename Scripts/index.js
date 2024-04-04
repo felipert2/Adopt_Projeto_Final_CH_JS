ad744c6b1bad0a5924f1060516f7c37b81315222
Bárbara Leão <barbaraleaobsc@gmail.com>
2024-04-04 10:41:31 -0300

    incluí a desestruturaçao no js do index

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,33 +1,11 @@
-/* var total = 0;
-
-document.getElementById("btnSomar").addEventListener("click", function() {
-    // Obtém o valor inserido pelo usuário
-    var valor = parseFloat(document.getElementById("inputValor").value);
-
-    // Verifica se o valor é numérico
-    if (!isNaN(valor)) {
-        // Adiciona o valor ao total
-        total += valor;
-
-        // Exibe o total acumulado
-        document.getElementById("resultado").innerText =  "R$" + total;
-    } else {
-        alert("Por favor, insira um valor numérico válido.");
-    }
-
-    // Limpa o campo de entrada
-    document.getElementById("inputValor").value = "";
-});
-*/
-
-// ABAIXO FIZ O DESAFIO COMPLEMENTAR DA ENTREGA DO DIA 27/03 
-// com operador ternário, desestruturação e alguns ajustes 
-
 let total = 0;
 
 document.getElementById("btnSomar").addEventListener("click", function () {
+    // desestruturação para extrair diretamente o valor do campo de entrada
+    const { value } = document.getElementById("inputValor");
+
     // confere o valor inserido pelo usuário e converte para número
-    const valor = parseFloat(document.getElementById("inputValor").value);
+    const valor = parseFloat(value);
 
     // verifica se o valor é numérico
     const isNumeric = !isNaN(valor);
